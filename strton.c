912db5b7c8ede9d1140f68d1d3b4d8c03977a01b
master-genius <1146040444@qq.com>
2018-07-25 16:12:05 +0800

    --

---
+++ strton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+    for (int i=1; i<argc; i++) {
+        printf ("%f %d \n", atof(argv[i]), atoi(argv[i]));
+    }
+    printf("\n");
+
+    return 0;
+}
