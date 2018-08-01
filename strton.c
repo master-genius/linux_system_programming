bf6fa34ec2f77b5334efadfa28e175df526df6a8
master-genius <1146040444@qq.com>
2018-08-02 00:46:57 +0800

    --

--- strton.c
+++ strton.c
@@ -4,8 +4,10 @@
 int main(int argc, char* argv[]) {
 
     for (int i=1; i<argc; i++) {
-        printf ("%f %d \n", atof(argv[i]), atoi(argv[i]));
+        printf("%f %d\n", strtod(argv[i], NULL), strtol(argv[i], NULL, 10));
+        //printf ("%f %d \n", atof(argv[i]), atoi(argv[i]));
     }
+
     printf("\n");
 
     return 0;
