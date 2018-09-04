a66e4ec1528f3ef75920a15b522a6cc1f8678f17
master-genius <1146040444@qq.com>
2018-09-04 18:00:55 +0800

    --

---
+++ ch07/manyfork.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <wait.h>
+
+#define PCS_PARENT  1
+#define PCS_CHILD   2
+#define PCS_UNSET   0
+
+int process_flag = PCS_UNSET;
+
+void handle_sig(int sig) {
+    printf("%d get signal: %u\n", getpid(), sig);
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    
+    process_flag = PCS_PARENT;
+
+    int child = 3;
+    pid_t pid;
+
+    for(int i=0; i<child; i++) {
+        pid = fork();
+
+        if (pid<0) {
+            perror("fork");
+            kill(0, SIGTERM);
+            return -1;
+        }
+
+        if (pid>0) {
+            continue;
+        } else {
+            process_flag = PCS_CHILD;
+            break;
+        }
+    }
+    if (process_flag == PCS_UNSET) {
+    
+    } else if (process_flag == PCS_PARENT) {
+        printf("parent:%d\n", getpid());
+        int status = 0;
+        for(int i=0;i<child; i++) {
+            printf("child %d, exit code: %d\n",wait(&status), status);
+        }
+    } else if (process_flag == PCS_CHILD) {
+        pid_t myid = getpid();
+        signal(SIGTERM, handle_sig);
+        printf("child:%d\n",myid);
+        sleep(myid % 10);
+        return (myid % 4);
+    }
+
+    return 0;
+}
+
