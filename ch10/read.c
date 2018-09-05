59cdd9ae8de78bcaaa1bc67f8c4056d91ef9d639
master-genius <1146040444@qq.com>
2018-09-05 08:08:19 +0800

    --

---
+++ ch10/read.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define DATA_BUF_LEN    2048
+
+int main(int argc, char *argv[]) {
+
+    char buffer[DATA_BUF_LEN+1] = {'\0', };
+
+    int fd = open("datatest", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    int count = 0;
+
+    count = read(fd, buffer, DATA_BUF_LEN);
+    if (count < 0) {
+        close(fd);
+        perror("read");
+        return 2;
+    }
+
+    printf("--read: \n%s\n--\n", buffer);
+    close(fd);
+
+    return 0;
+}
+
