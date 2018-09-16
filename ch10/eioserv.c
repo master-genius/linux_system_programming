af5c2528353019060aa0f9b588c785b59e5ed198
master-genius <1146040444@qq.com>
2018-09-16 18:00:35 +0800

    --

--- ch10/eioserv.c
+++ ch10/eioserv.c
@@ -18,21 +18,28 @@
 
 #define DEF_PORT    2018
 
-#define MAX_EVENTS  1024
+#define MAX_EVENTS  1024        //接收的最大请求
 
-#define BUFF_SIZE   1001
+#define BUFF_SIZE   1001        
 
-#define MAX_MSG_SIZE    8192
+#define MAX_MSG_SIZE    8192    //单个消息最大字节数
 
+/*
+    消息状态 
+*/
 #define RECV_MSG_NONE   0
 #define RECV_MSG_ERR    1
 #define RECV_MSG_OK     2
 #define RECV_MSG_CLOSE  3
 
+/* 标记父进程和子进程 */
 #define PCS_CHILD   1
 #define PCS_PARENT  2
 
-#define ACCPET_MAX_LIMIT    3
+#define CHILD_COUNT     2
+
+#define ACCEPT_MAX_LIMIT    MAX_EVENTS/CHILD_COUNT
+
 
 union semun {
     int val;
@@ -41,13 +48,34 @@ union semun {
     struct seminfo *__buf;
 };
 
-key_t _save_semid = 0;
+key_t _save_semid = 0;      //保存信号量ID
 
-void handle_sig(int sig) {
+pid_t _self_pid;
+int _accpet_count;  //accpet计数
+int __child_count = 0;
+
+void parent_handle_sig(int sig) {
     if (sig == SIGINT || sig == SIGTERM) {
         printf("remove semid\n");
         semctl(_save_semid, 0, IPC_RMID);
-        kill(-1, SIGTERM);
+        kill(0, SIGTERM);
+        usleep(50000);
+        exit(0);
+    } else if (sig == SIGCHLD) {
+        int st;
+        pid_t pid;
+
+        while((pid=waitpid(0, &st, WNOHANG)) > 0) {
+            if (WIFEXITED(st) || WIFSIGNALED(st)) {
+                printf("child %d exited\n", pid);
+                __child_count -= 1;
+            }
+        }
+    }
+}
+
+void child_handle_sig(int sig) {
+    if (sig == SIGTERM || sig == SIGINT) {
         exit(0);
     }
 }
@@ -80,12 +108,13 @@ struct event_msg {
     struct msg_block data;
 };
 
-struct event_msg _evtmsg[MAX_EVENTS];
+//全局信息存储
+struct event_msg _evtmsg[ACCEPT_MAX_LIMIT];
 
 
-pid_t _self_pid;
-int _accpet_count;
 
+
+//设置套接字的文件描述符为非阻塞模式
 int set_nonblocking(int fd) {
     int old_opt = fcntl(fd, F_GETFL);
     int new_opt = old_opt | O_NONBLOCK;
@@ -95,6 +124,7 @@ int set_nonblocking(int fd) {
     return old_opt;
 }
 
+//新创建的套接字连接加入到epoll列表中
 void ep_addfd(int efd, int fd, int et){
     struct epoll_event evt;
     evt.data.fd = fd;
@@ -106,11 +136,13 @@ void ep_addfd(int efd, int fd, int et){
     set_nonblocking(fd);
 }
 
+//从epoll连接列表中删除套接字
 void ep_delfd(int efd, int fd) {
     struct epoll_event evt;
     epoll_ctl(efd, EPOLL_CTL_DEL, fd, &evt);
 }
 
+//设置套接字的触发模式
 void ep_modfd(int efd, int fd, int opt, int et) {
     struct epoll_event evt;
     evt.data.fd = fd;
@@ -120,8 +152,10 @@ void ep_modfd(int efd, int fd, int opt, int et) {
     epoll_ctl(efd, EPOLL_CTL_MOD, fd, &evt);
 }
 
+
+
 void save_msg(int fd, struct msg_block * msg) {
-    int mi = fd % MAX_EVENTS;
+    int mi = fd % ACCEPT_MAX_LIMIT;
     int fi = -1;
 
     if ( (_evtmsg[mi].inuse && _evtmsg[mi].fd == fd)
@@ -130,14 +164,14 @@ void save_msg(int fd, struct msg_block * msg) {
     ) {
         fi = mi;
     } else {
-        for (int i=0; i<MAX_EVENTS; i++) {
+        for (int i=0; i<ACCEPT_MAX_LIMIT; i++) {
             if (_evtmsg[i].inuse==0) {
                 fi = i;
                 break;
             }
         }
     }
-    if (fi>=0 && fi<MAX_EVENTS) {
+    if (fi>=0 && fi<ACCEPT_MAX_LIMIT) {
         _evtmsg[fi].inuse = 1;
         _evtmsg[fi].fd = fd;
         strcpy(_evtmsg[mi].data.msg, msg->msg);
@@ -145,12 +179,17 @@ void save_msg(int fd, struct msg_block * msg) {
 }
 
 struct event_msg * get_msg(int fd) {
-    for(int i=0;i<MAX_EVENTS;i++) {
+    for(int i=0;i<ACCEPT_MAX_LIMIT;i++) {
         if (_evtmsg[i].fd == fd) {
             return (_evtmsg+i);
         }
     }
     return NULL;
+}
+
+void close_socket(int fd) {
+    close(fd);
+    _accpet_count -= 1;
 }
 
 void event_et(struct epoll_event *evt, int number, int efd, int lisd) {
@@ -164,7 +203,7 @@ void event_et(struct epoll_event *evt, int number, int efd, int lisd) {
 
     for (int i=0; i<number; i++) {
         if (lisd == evt[i].data.fd) {
-            if (_accpet_count >= ACCPET_MAX_LIMIT)
+            if (_accpet_count >= ACCEPT_MAX_LIMIT)
                 return;
             if (try_accpet_lock() < 0) {
                 printf("%u : failed to get lock\n", getpid());
@@ -201,14 +240,12 @@ void event_et(struct epoll_event *evt, int number, int efd, int lisd) {
                         break;
                     }
                     recv_flag = RECV_MSG_ERR;
-                    close(evt[i].data.fd);
-                    _accpet_count -= 1;
+                    close_socket(evt[i].data.fd);
                     break;
                 } else if (recv_count == 0) { //connection closed
                     recv_flag = RECV_MSG_CLOSE;
                     ep_delfd(efd, evt[i].data.fd);
-                    close(evt[i].data.fd);
-                    _accpet_count -= 1;
+                    close_socket(evt[i].data.fd);
                     break;
                 } else {
                     msg_count += recv_count;
@@ -223,8 +260,7 @@ void event_et(struct epoll_event *evt, int number, int efd, int lisd) {
 
                 if (strncmp(msg.msg, "//quit", 6)==0) {
                     ep_delfd(efd, evt[i].data.fd);
-                    close(evt[i].data.fd);
-                    _accpet_count -= 1;
+                    close_socket(evt[i].data.fd);
                     return ;
                 }
                 save_msg(evt[i].data.fd, &msg);
@@ -241,8 +277,6 @@ void event_et(struct epoll_event *evt, int number, int efd, int lisd) {
             printf("send msg: %s\n", emsg->data.msg);
             send(evt[i].data.fd, (void*)emsg->data.msg, strlen(emsg->data.msg), 0);
             ep_modfd(efd, evt[i].data.fd, EPOLLIN, 1);
-            //ep_delfd(efd, evt[i].data.fd);
-            //close(evt[i].data.fd);
         
         }else {
             printf("nothing to do\n");
@@ -250,14 +284,26 @@ void event_et(struct epoll_event *evt, int number, int efd, int lisd) {
     }
 }
 
-#define CHILD_COUNT     3
 
 int main(int argc, char * argv[]) {
 
+    //start to daemon
+    
+    pid_t dpid = fork();
+    if (dpid<0) {
+        perror("fork");
+        return 1;
+    }
+    if (dpid > 0) {
+        exit(0);
+    }
+    setsid();
+
+    //end daemon
+
     int process_flag = PCS_PARENT;
 
-    //set semaphore
-    //
+    //start to set semaphore
     key_t semk = 1024;
     int semid;
     semid = semget(semk, 1, IPC_CREAT|0666);
@@ -289,6 +335,7 @@ int main(int argc, char * argv[]) {
 
     //end sem
 
+
     struct sockaddr_in addr;
 
     int lisd = socket(PF_INET, SOCK_STREAM, 0);
@@ -313,7 +360,7 @@ int main(int argc, char * argv[]) {
         perror("bind");
         return -1;
     }
-    if(listen(lisd,12))
+    if(listen(lisd,13))
     {
         perror("listen");
         return -1;
@@ -323,11 +370,12 @@ int main(int argc, char * argv[]) {
     for(int i=0; i<CHILD_COUNT; i++) {
         pid = fork();
         if (pid < 0) {
-            kill(-1, SIGTERM);
+            kill(0, SIGTERM);
             semctl(semid, 0, IPC_RMID);
-        }
-
-        if (pid > 0) {
+            perror("fork");
+            return 1;
+        } else if (pid > 0) {
+            __child_count += 1;
             continue;
         } else {
             process_flag = PCS_CHILD;
@@ -335,21 +383,26 @@ int main(int argc, char * argv[]) {
         }
     }
 
-    _self_pid = getpid();
 
     if (process_flag == PCS_PARENT) {
-        signal(SIGINT, handle_sig);
-        signal(SIGTERM, handle_sig);
-        int st;
-        for(int i=0; i<CHILD_COUNT; i++) {
-            printf("%d exited\n", wait(&st));
+        
+        _self_pid = getpid();
+        printf("daemon running:%u\n", _self_pid);
+
+        signal(SIGINT, parent_handle_sig);
+        signal(SIGTERM, parent_handle_sig);
+        signal(SIGCHLD, parent_handle_sig);
+        while (1) {
+            sleep(2);
         }
 
     } else {
+        _self_pid = getpid();
         signal(SIGINT, SIG_IGN);
+        signal(SIGTERM, child_handle_sig);
 
         _accpet_count = 0;
-        struct epoll_event events[MAX_EVENTS];
+        struct epoll_event events[ACCEPT_MAX_LIMIT];
 
         int efd = epoll_create1(0);
         if (efd < 0) {
@@ -359,10 +412,9 @@ int main(int argc, char * argv[]) {
 
         ep_addfd(efd, lisd, 1);
 
-
         int event_count = 0;
         while(1) {
-            event_count = epoll_wait(efd, events, MAX_EVENTS, -1);
+            event_count = epoll_wait(efd, events, ACCEPT_MAX_LIMIT, -1);
             if (event_count < 0) {
                 perror("epoll_wait");
                 break;
--- test.c
+++ test.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
-#include <wchar_t.h>
 
+#define A_X     12
+#define A_Y     13
+
+#define X_D_Y   A_Y/A_X
 
 int main(void) {
+    printf("%d\n", X_D_Y);
 
-    char fmt_relt[512] = {'\0',};
-    char fmtstr[128] = {'\0',};
-
-    sprintf(fmtstr, "%%%ds", 64);
-
-    sprintf(fmt_relt, fmtstr, "Linux is great\n");
-    
-    printf("%s", fmt_relt);
+//    char fmt_relt[512] = {'\0',};
+//    char fmtstr[128] = {'\0',};
+//
+//    sprintf(fmtstr, "%%%ds", 64);
+//
+//    sprintf(fmt_relt, fmtstr, "Linux is great\n");
+//    
+//    printf("%s", fmt_relt);
 
     return 0;
 }
