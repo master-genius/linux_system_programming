25cf62137e9ea7f39afdd41644581478d08b381c
master-genius <1146040444@qq.com>
2018-09-27 23:31:51 +0800

    --

--- ch03/qsortbig.c
+++ ch03/qsortbig.c
@@ -6,6 +6,27 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
+#define HELP_INFO   "\
+    排序性能测试程序：\n\
+        对比插入排序和快速排序\n\
+        程序默认随机生成10000个整数进行快速排序\n\
+        --time   :  输出排序运行时间\n\
+        --sort   :  设定排序方式，支持insert和quick\n\
+        --no-out :  不输出排序结果\n\
+        [NUMBER] :  设定一个要随机生成的整数个数\n\
+    示例：\n\
+        随机生成5000个数进行插入排序：\n\
+            sortbig --sort=insert 5000\n\
+        随机生成20000个数进行快速排序并输出运行时间：\n\
+            sortbig 20000 --time [默认就是进行快速排序]\n\
+        不输出排序结果：\n\
+            sortbig --no-out 15000 --time --sort=insert\n\
+"
+
+void help(void) {
+    printf("%s\n", HELP_INFO);
+}
+
 void qsorti(int *d, int start, int end) {
     if (start >= end) {
         return ;
@@ -67,7 +88,10 @@ int main(int argc, char *argv[])
 
     if (argc>1) {
         for(int i=1;i<argc;i++) {
-            if (strncmp(argv[i],"--sort=",7)==0) {
+            if (strcmp(argv[i], "--help") == 0) {
+                help();
+                return 0;
+            } else if (strncmp(argv[i],"--sort=",7)==0) {
                 if (strcmp(argv[i]+7, "insert")==0) {
                     ARGS[ARGS_SORT] = SORT_INSERT;
                 } else if(strcmp(argv[i]+7,"quick")==0) {
@@ -96,9 +120,13 @@ int main(int argc, char *argv[])
     printf("creating random number...\n");
     srand(time(NULL));
     for(int i=0;i<N;i++) {
-        nms[i] = i + (rand() % 1024);
+        nms[i] = (N-i) + (rand() % 1024);
         srand(i);
     }
+
+    /*
+        获取开始排序的时间
+    */
     struct timeval  start_time,end_time;
     gettimeofday(&start_time,0);
 
@@ -107,6 +135,10 @@ int main(int argc, char *argv[])
     } else {
         insert_sort(nms, N);
     }
+
+    /*
+        获取结束时间计算运行时间
+    */
     gettimeofday(&end_time,0);
     double run_sec = (double)(end_time.tv_sec - start_time.tv_sec);
     double run_usec = ((double)(end_time.tv_usec - start_time.tv_usec)/1000000);
@@ -114,11 +146,12 @@ int main(int argc, char *argv[])
     if (ARGS[ARGS_OUT]) {
         for (int i=0;i<N;i++) {
             printf("%-6d ", nms[i]);
-            if (i > 0 && i%6==0)
+            if ( ( (i+1)%6 ) ==0 )
                 printf("\n");
         }
         printf("\n");
     }
+
     if (ARGS[ARGS_TIME]) {
         printf("--run time : %g s\n", run_sec+run_usec);
     }
